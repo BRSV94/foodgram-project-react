9d60c4d44090857f578008c8e22d46f9440b57d5
Sergey Borisov <brsv-s@yandex.ru>
2024-04-07 18:20:04 +0300

    add import data util

---
+++ backend/recipes/utils.py
@@ -0,0 +1,19 @@
+import json
+from .models import Ingredient, MeasurementUnit
+
+
+def import_data():
+    with open('../../data/ingredients.json', 'r') as file:
+        data = json.load(file)
+        for ingredient in data:
+            # {"name": "абрикосовое варенье", "measurement_unit": "г"}
+            name = ingredient['name']
+            measurement_unit = ingredient['measurement_unit']
+            unit_obj = MeasurementUnit.objects.get_or_create(
+                measurement_unit=measurement_unit
+            )
+            Ingredient.objects.get_or_create(
+                name=name,
+                measurement_unit=unit_obj
+            )
+    print('Данные ингредиентов успешно добавлены в бд.')
