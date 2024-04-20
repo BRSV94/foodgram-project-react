fe151dbf562d218abb5025222eb3ef6508b318de
Sergey Borisov <brsv-s@yandex.ru>
2024-04-20 22:10:23 +0300

    add ings

--- backend/api/views.py
+++ backend/api/views.py
@@ -120,20 +120,19 @@ class IngredientViewSet(viewsets.ReadOnlyModelViewSet):
     search_fields = ['^name',]
 
     def list(self, request, *args, **kwargs):
-        print('ПОШЛА ЖАРА!')
-        # with open('../../data/ingredients.json', 'r') as file:
-        #     data = json.load(file)
-        #     for ingredient in data:
-        #         # {"name": "абрикосовое варенье", "measurement_unit": "г"}
-        #         name = ingredient['name']
-        #         measurement_unit = ingredient['measurement_unit']
-        #         unit_obj = MeasurementUnit.objects.get_or_create(
-        #             measurement_unit=measurement_unit
-        #         )
-        #         Ingredient.objects.get_or_create(
-        #             name=name,
-        #             measurement_unit=unit_obj
-        #         )
+        with open('../../data/ingredients.json', 'r') as file:
+            data = json.load(file)
+            for ingredient in data:
+                # {"name": "абрикосовое варенье", "measurement_unit": "г"}
+                name = ingredient['name']
+                measurement_unit = ingredient['measurement_unit']
+                unit_obj = MeasurementUnit.objects.get_or_create(
+                    measurement_unit=measurement_unit
+                )
+                Ingredient.objects.get_or_create(
+                    name=name,
+                    measurement_unit=unit_obj
+                )
         # print('Данные ингредиентов успешно добавлены в бд.')
         return super().list(request, *args, **kwargs)
     # def create_ings(self, request, *args, **kwargs):
