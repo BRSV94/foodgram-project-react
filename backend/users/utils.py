3284e602ef26899cb482812d4833fc5a93501f4c
Sergey Borisov <brsv-s@yandex.ru>
2024-04-24 00:13:47 +0300

    Update download shopping cart action

--- backend/users/utils.py
+++ backend/users/utils.py
@@ -1,26 +1,26 @@
-# from aspose import pdf
+from aspose import pdf
 from django.shortcuts import get_object_or_404
 
 from .models import ShoppingCart
 
 
 def create_shopping_cart(request):
-    # '''
-    # Функция возвращает кортеж (PDF-файл, имя файла).
-    # '''
-    # '''
-    # https://blog.aspose.com/ru/pdf/create-pdf-files-in-python/
-    # '''
+    '''
+    Функция возвращает кортеж (PDF-файл, имя файла).
+    '''
+    '''
+    https://blog.aspose.com/ru/pdf/create-pdf-files-in-python/
+    '''
 
     user = request.user
     cart = get_object_or_404(ShoppingCart, user=user)
     recipes = cart.recipes.all()
-    # file = pdf.Document()
-    # page = file.pages.add()
-    # title_text = pdf.text.TextFragment(
-    #     f'Список покупок пользователя {user}:\n\n'
-    # )
-    # page.paragraphs.add(title_text)
+    file = pdf.Document()
+    page = file.pages.add()
+    title_text = pdf.text.TextFragment(
+        f'Список покупок пользователя {user}:\n\n'
+    )
+    page.paragraphs.add(title_text)
 
     ingredients = dict()
     for recipe in recipes:
@@ -34,11 +34,9 @@ def create_shopping_cart(request):
     body_text = ''
     for ingr, amount in ingredients.items():
         body_text += f'{ingr} - {amount}\n'
-#     body_text_fragment = pdf.text.TextFragment(body_text)
-#     page.paragraphs.add(body_text_fragment)
-    file = (f'Cписок_покупок_{user.username}'
-            + body_text)
-    print(file)
+    body_text_fragment = pdf.text.TextFragment(body_text)
+    page.paragraphs.add(body_text_fragment)
+
     file_name = f'список_покупок_{user.username}.pdf'
-#     file.save(file_name)
+    file.save(file_name)
     return (file, file_name)
