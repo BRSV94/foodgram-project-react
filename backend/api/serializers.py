a2d9339f4d7fd53fa4546eb8100fead8308bfdd4
Sergey Borisov <brsv-s@yandex.ru>
2024-05-02 12:44:50 +0300

    update after review

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -194,7 +194,7 @@ class RecipeReadSerializer(ModelSerializer):
 class RecipeWriteSerializer(ModelSerializer):
     # tags = TagListField(queryset=Tag.objects.all(), many=True)
     # tags = TagSerializer(many=True)
-    tags = PrimaryKeyRelatedField(many=True)
+    tags = PrimaryKeyRelatedField(queryset=Tag.objects.all(), many=True)
     author = UserSerializer(read_only=True) # Read only?
     ingredients = IngredientInRecipeWriteSerializer(many=True)
     # ingredients = IngredientInRecipeWriteSerializer(many=True)
